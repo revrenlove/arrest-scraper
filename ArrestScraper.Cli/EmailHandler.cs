378851512c2719281cc01c999578efa2a00835c8
revrenlove <elrd.dev@gmail.com>
2023-08-14 19:20:01 -0500

    fixing settings

--- ArrestScraper.Cli/EmailHandler.cs
+++ ArrestScraper.Cli/EmailHandler.cs
@@ -8,7 +8,6 @@ public class EmailHandler
     private const string SmtpHost = "smtp.gmail.com";
     private const int SmtpPort = 587;
 
-
     public EmailHandler(string emailAddress, string key)
     {
         EmailAddress = emailAddress;
@@ -34,31 +33,3 @@ public class EmailHandler
         smtpClient.Send(message);
     }
 }
-
-// using System.Net;
-// using System.Net.Mail;
-
-// // Email message info
-// var toEmailAddress = "Recipient's email address";
-// var subjectLine = "Test Subject Line 1002";
-// var emailBody = "Test Email Body 1002";
-
-// // Your Email Credentials
-// var emailAddress = "Your Email Address";
-// var emailPassword = "Your password or app key";
-
-// var smtpHost = "smtp.gmail.com";
-// var smtpPort = 587;
-
-// var smtpClient = new SmtpClient
-// {
-//     Host = smtpHost,
-//     Port = smtpPort,
-//     EnableSsl = true,
-//     DeliveryMethod = SmtpDeliveryMethod.Network,
-//     UseDefaultCredentials = false,
-//     Credentials = new NetworkCredential(emailAddress, emailPassword)
-// };
-
-// using var message = new MailMessage(emailAddress, toEmailAddress, subjectLine, emailBody);
-// smtpClient.Send(message);
--- ArrestScraper.Cli/Program.cs
+++ ArrestScraper.Cli/Program.cs
@@ -4,31 +4,38 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 
-AppSettings appSettings =
-    new ConfigurationBuilder()
-        .AddJsonFile("appsettings.json")
-        .Build()
-        .Get<AppSettings>()!;
+internal class Program
+{
+    private static async Task Main(string[] args)
+    {
+        AppSettings appSettings =
+            new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .AddUserSecrets<Program>()
+                .Build()
+                .Get<AppSettings>()!;
 
-var timespan = TimeSpan.FromMinutes(appSettings.FrequencyInMinutes);
-var timer = new PeriodicTimer(timespan);
+        var timespan = TimeSpan.FromMinutes(appSettings.FrequencyInMinutes);
+        var timer = new PeriodicTimer(timespan);
 
-var serviceProvider =
-    new ServiceCollection()
-        .AddLogging(configure =>
-        {
-            configure.AddConsole();
-        })
-        .AddSingleton<ArrestScraperService>()
-        .AddSingleton<EmailHandler>(serviceProvider =>
-        {
-            return new(appSettings.EmailCredentials.EmailAddress, appSettings.EmailCredentials.Key);
-        })
-        .AddSingleton<HttpClient>()
-        .AddSingleton(timer)
-        .AddSingleton(appSettings)
-        .BuildServiceProvider();
+        var serviceProvider =
+            new ServiceCollection()
+                .AddLogging(configure =>
+                {
+                    configure.AddConsole();
+                })
+                .AddSingleton<ArrestScraperService>()
+                .AddSingleton<EmailHandler>(serviceProvider =>
+                {
+                    return new(appSettings.EmailCredentials.EmailAddress, appSettings.EmailCredentials.Key);
+                })
+                .AddSingleton<HttpClient>()
+                .AddSingleton(timer)
+                .AddSingleton(appSettings)
+                .BuildServiceProvider();
 
-var arrestScraperService = serviceProvider.GetService<ArrestScraperService>()!;
+        var arrestScraperService = serviceProvider.GetService<ArrestScraperService>()!;
 
-await arrestScraperService.Execute();
+        await arrestScraperService.Execute();
+    }
+}
